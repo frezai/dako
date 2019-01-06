bb04dc684f082a9f4be14a67834b20e491aacf31
Fatma Rezai <frezai@hm.edu>
2019-01-06 16:26:00 +0100

    neu

--- chatApplication/src/main/java/edu/hm/dako/chat/server/UDPAuditServer.java
+++ chatApplication/src/main/java/edu/hm/dako/chat/server/UDPAuditServer.java
@@ -10,32 +10,34 @@ import java.sql.Timestamp;
  *
  */
 public class UDPAuditServer {
-	
+
 	public static void main(String[] args) throws Exception {
 		System.out.println("UDP-Server wird gestartet...");
-		
+
 		// Erstellte Textdatei
 		PrintWriter printer = new PrintWriter("UDP-file.txt", "UTF-8");
-		
-		DatagramSocket socket = new DatagramSocket (9876);
-		
+
+		DatagramSocket socket = new DatagramSocket(9876);
+
 		while (true) {
-		byte buffer[] = new byte[6553500];
-		DatagramPacket packet = new DatagramPacket (buffer, buffer.length);
-		socket.receive(packet);
-		String sentence = new String(packet.getData()).trim();
-		System.out.println ("Daten erhalten: " + sentence);
-		printer.println(sentence);
-		printer.flush();
-		
-		//Shutdown-Nachricht, um den AuditLog-Server zu beenden.
-		if (sentence.contains("Undefined")) {
-			System.out.println("UDP AuditLog Server wird heruntergefahren " + new Timestamp(System.currentTimeMillis()));
-			printer.println("UDP AuditLog Server wird heruntergefahren " + new Timestamp(System.currentTimeMillis()));
+			byte buffer[] = new byte[6553500];
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			socket.receive(packet);
+			String sentence = new String(packet.getData()).trim();
+			System.out.println("Daten erhalten: " + sentence);
+			printer.println(sentence);
 			printer.flush();
-			printer.close();
-			System.exit(0);
-		}
+
+			// Shutdown-Nachricht, um den AuditLog-Server zu beenden.
+			if (sentence.contains("Undefined")) {
+				System.out.println(
+						"UDP AuditLog Server wird heruntergefahren " + new Timestamp(System.currentTimeMillis()));
+				printer.println(
+						"UDP AuditLog Server wird heruntergefahren " + new Timestamp(System.currentTimeMillis()));
+				printer.flush();
+				printer.close();
+				System.exit(0);
+			}
 		}
 	}
 }
